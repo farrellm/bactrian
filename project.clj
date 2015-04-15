d139a0ada5d2c7be409fa9d5c9b010f37d1faca3
Matthew Farrell <farrellm@alum.mit.edu>
2015-04-14 23:53:46 -0400

    dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject bactrian "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "ADT"
+  :url "https://github.com/farrellm/bactrian"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.typed "0.2.84"]
+                 [org.clojure/core.match "0.3.0-alpha4"]])
