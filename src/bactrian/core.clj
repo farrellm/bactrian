a05b36d43168487e1fcbfa2cbb02e79b6f5cc57b
Matthew Farrell <farrellm@alum.mit.edu>
2015-04-18 21:13:54 -0400

    untyped

--- src/bactrian/core.clj
+++ src/bactrian/core.clj
@@ -1,6 +1,24 @@
 (ns bactrian.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn gen-defs [[name args]]
+  (if (number? args)
+    (let [arg-names (map #(symbol (str "a" %))
+                         (range args))]
+      `(defn ~name [ ~@arg-names ]
+         [~(keyword name) ~@arg-names]))
+    (if (empty? args)
+      `(defn ~name []
+         [~(keyword name)])
+      (let [arg-names (map #(symbol (str "a__" %))
+                           args)
+            arg-pairs (map #(vector (keyword %1) %2)
+                           args arg-names)]
+        `(defn ~name [ ~@arg-names ]
+           [~(keyword name) ~(into {} arg-pairs)])))))
+
+(defn defadt* [type-name body]
+  (let [defs (map gen-defs (partition 2 body))]
+    `(do ~@defs)))
+
+(defmacro defadt [name & body]
+  (defadt* name body))
--- test/bactrian/core_test.clj
+++ test/bactrian/core_test.clj
@@ -1,7 +1,29 @@
 (ns bactrian.core-test
   (:require [clojure.test :refer :all]
-            [bactrian.core :refer :all]))
+            [bactrian.core :refer :all]
+            [clojure.core.match :refer [match]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defadt Maybe
+  nothing 0
+  just 1)
+
+(defadt Pair
+  root []
+  pair [car cdr])
+
+(def j (just 8))
+(def n (nothing))
+(def r (root))
+(def p (pair 1 2))
+
+(deftest maybe-test
+  (testing "just"
+    (is (= 8 (match j [:just x] x [:nothing] 0))))
+  (testing "nothing"
+    (is (= 0 (match n [:just x] x [:nothing] 0)))))
+
+(deftest pair-test
+  (testing "pair"
+    (is (= 1 (match p [:pair {:car a, :cdr b}] a [root] 0))))
+  (testing "root"
+    (is (= 0 (match r [:pair {:car a, :cdr b}] a [root] 0)))))
